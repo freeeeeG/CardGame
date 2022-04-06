f1f7c565f22b64fd551326b689a4fe3c882137db
freeeeeG <1284566797@qq.com>
2022-04-06 20:53:31 +0800

    haoye

---
+++ Script/CamareManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamareManager : MonoBehaviour
+{
+
+
+    public GameObject camarePoint;
+    public Vector3 mousePos;
+
+    public float speed_k = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        mousePos = Input.mousePosition;
+        
+
+        camarePoint.transform.position = new Vector3((mousePos.x-960f)/speed_k,camarePoint.transform.position.y, camarePoint.transform.position.z);
+        Debug.Log(mousePos);
+    }
+}
+    
